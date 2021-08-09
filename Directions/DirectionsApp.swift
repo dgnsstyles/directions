f5c95d004ca22b6c29ae9a10eb2e8bf694a5c20a
David Goren <david.goren_ex@scotiabank.cl>
2021-08-09 16:14:18 -0400

    Initial Commit

---
+++ Directions/DirectionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  DirectionsApp.swift
+//  Directions
+//
+//  Created by usuario on 09-08-21.
+//
+
+import SwiftUI
+
+@main
+struct DirectionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
